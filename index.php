61aae45e33f68802db6cc07327e9b19b075b38f6
Tcharlles <tcharlles3@gmail.com>
2023-09-13 20:30:47 -0300

    working-new-questions

--- index.php
+++ index.php
@@ -3,43 +3,79 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
   <title>Radio box test</title>
 </head>
 <body>
-  <h1>Teste radio buttons</h1>
   
-  <form action="php/create.php" method="post">
-    <fieldset>
-      <legend>Create</legend><br>
-      <mark>
-          <?php if (isset($_GET['ms'])){
-            echo $_GET['ms'];
-          } ?>
-      </mark>
+  <section class="container">
+    <h1>Estamos quase lá...</h1>
+    <p>Responda às perguntas e consiga uma condição especial</p>
+    <form action="php/create.php" method="post">
       <div>
-        <label> Name</label>
-        <input type="text" name="name">
-      </div><br>
+        <mark>
+            <?php if (isset($_GET['ms'])){
+              echo $_GET['ms'];
+            } ?>
+        </mark>
+        <div>
+          <label> Gênero</label>
+          <input type="radio" name="gender" value="Masculino" checked >
+          <label>Masculino</Label>
 
-      <div>
-        <label> Gender</label>
-        <input type="radio" name="gender" value="Male" checked >
-        <label>Male</Label>
+          <input type="radio" name="gender" value="Feminino">
+          <label>Feminino</Label>
+        </div><br>
 
-        <input type="radio" name="gender" value="Female">
-        <label>Female</Label>
-      </div><br>
+        <div>
+          <label> Sua renda</label>
+          <input type="radio" name="renda" value="R$500" checked>
+          <label>R$500</Label>
 
-      <div>
-        <input type="checkbox" name="programmer">
-        <label>Are you a programmer?</label>
-      </div><br>
-      <input type="hidden" name="timestamp" value="<?php echo date('Y-m-d H:i:s'); ?>">
-      <input type="submit" value="Create">
-      <a href="read.php">View Users</a>
+          <input type="radio" name="renda" value="R$1000">
+          <label>R$1000</Label>
 
-    </fieldset>
-  </form>
+          <input type="radio" name="renda" value="R$3000">
+          <label>R$3000</Label>
+        </div><br>
+
+        <div>
+          <label> Qual sua principal dor ligada a relacionamentos?</label>
+          <input type="radio" name="dor" value="Dependencia" >
+          <label>Dependencia</Label>
+
+          <input type="radio" name="dor" value="Superar relacionamentos" checked>
+          <label>Superar relacionamentos</Label>
+          
+          <input type="radio" name="dor" value="Relacionamento abusivo">
+          <label>Viver em um relacionamento abusivo</Label>
+        </div><br>
+
+        <div>
+          <label> Você está pronto para mudar de vida?</label>
+          <input type="radio" name="mudar" value="Sim" >
+          <label>Sim</Label>
+
+          <input type="radio" name="mudar" value="Não" checked>
+          <label>Não, prefiro continuar sofrendo</Label>
+        </div><br>
+
+        <div>
+          <label> Você faz terapia atualmente?</label>
+          <input type="radio" name="terapia" value="Sim" checked >
+          <label>Sim</Label>
+
+          <input type="radio" name="terapia" value="Não">
+          <label>Não</Label>
+        </div><br>
+
+        <input type="hidden" name="timestamp" value="<?php echo date('Y-m-d H:i:s'); ?>">
+        <input type="submit" value="Create">
+        <a href="read.php">View Users</a>
+
+      </div>
+    </form>
+  </section>
   
 </body>
 </html>
--- php/create.php
+++ php/create.php
@@ -1,42 +1,40 @@
 <?php
+if (
+    isset($_POST['gender']) &&
+    isset($_POST['renda']) &&
+    isset($_POST['dor']) &&
+    isset($_POST['mudar']) &&
+    isset($_POST['terapia'])
+) {
+    include "../db_conn.php";
 
-if (isset($_POST['name']) && isset($_POST['gender'])) {
-  include "../db_conn.php";
+    $gender = $_POST['gender'];
+    $renda = $_POST['renda'];
+    $dor = $_POST['dor'];
+    $mudar = $_POST['mudar'];
+    $terapia = $_POST['terapia'];
 
-  $name = $_POST['name'];
-  $gender = $_POST['gender'];
+    // Defina o fuso horário local
+    date_default_timezone_set('America/Sao_Paulo'); // Substitua 'America/Sao_Paulo' pelo fuso horário da sua localização
 
-  if (empty($name)) {
-      header("Location: ../index.php?ms=Name is required");
-      exit;
-  } else {
-      if (isset($_POST['programmer'])) {
-          $programmer = "YES";
-      } else {
-          $programmer = "NO";
-      }
+    // Capturar a data e hora atual
+    $timestamp = date('Y-m-d H:i:s');
 
-      // Defina o fuso horário local
-      date_default_timezone_set('America/Sao_Paulo'); // Substitua 'America/Sao_Paulo' pelo fuso horário da sua localização
+    $sql = "INSERT INTO users (gender, renda, dor, mudar, terapia, timestamp) 
+            VALUES ('$gender', '$renda', '$dor', '$mudar', '$terapia', '$timestamp')";
+    $result = mysqli_query($conn, $sql);
 
-      // Capturar a data e hora atual
-      $timestamp = date('Y-m-d H:i:s');
-     
-
-      $sql = "INSERT INTO users (name, gender, programmer, timestamp) VALUES ('$name', '$gender', '$programmer', '$timestamp')";
-      $result = mysqli_query($conn, $sql);
-
-      if ($result) {
-          $ms = "Successfully Created";
-          header("Location: ../index.php?ms=$ms");
-          exit;
-      } else {
-          $ms = "Unknown Error occurred";
-          header("Location: ../index.php?ms=$ms");
-          exit;
-      }
-  }
+    if ($result) {
+        $ms = "Successfully Created";
+        header("Location: ../index.php?ms=$ms");
+        exit;
+    } else {
+        $ms = "Unknown Error occurred";
+        header("Location: ../index.php?ms=$ms");
+        exit;
+    }
 } else {
-  header("Location: ../index.php");
-  exit;
+    header("Location: ../index.php");
+    exit;
 }
+?>
