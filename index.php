66aa039aca0df63f578b9b62abf52efd076c7f1f
Tcharlles <tcharlles3@gmail.com>
2023-09-13 21:52:34 -0300

    working

--- index.php
+++ index.php
@@ -7,75 +7,110 @@
   <title>Radio box test</title>
 </head>
 <body>
-  
   <section class="container">
     <h1>Estamos quase lá...</h1>
     <p>Responda às perguntas e consiga uma condição especial</p>
-    <form action="php/create.php" method="post">
-      <div>
+    <a href="read.php">View Users</a>
+
+    <form action="php/create.php" method="post" id="question-form">
+      <div id="question1">
+        <mark>
+          <?php if (isset($_GET['ms'])){
+            echo $_GET['ms'];
+          } ?>
+        </mark>
         <div>
-          <label> Gênero</label>
-          <input type="radio" name="gender" value="Masculino" checked >
-          <label>Masculino</Label>
+          <label>Gênero</label>
+          <input type="radio" name="gender" value="Masculino" checked>
+          <label>Masculino</label>
 
           <input type="radio" name="gender" value="Feminino">
-          <label>Feminino</Label>
+          <label>Feminino</label>
         </div><br>
+        <!-- Adicione um botão para avançar para a próxima pergunta -->
+        <button type="button" onclick="showQuestion(2)">Próximo</button>
+      </div>
 
+      <div id="question2" style="display: none;">
+        <!-- Pergunta 2 -->
         <div>
-          <label> Sua renda</label>
+          <label>Sua renda</label>
           <input type="radio" name="renda" value="R$500" checked>
-          <label>R$500</Label>
+          <label>R$500</label>
 
           <input type="radio" name="renda" value="R$1000">
-          <label>R$1000</Label>
+          <label>R$1000</label>
 
           <input type="radio" name="renda" value="R$3000">
-          <label>R$3000</Label>
+          <label>R$3000</label>
         </div><br>
+        <!-- Adicione um botão para avançar para a próxima pergunta -->
+        <button type="button" onclick="showQuestion(3)">Próximo</button>
+      </div>
 
+      <div id="question3" style="display: none;">
+        <!-- Pergunta 3 -->
         <div>
-          <label> Qual sua principal dor ligada a relacionamentos?</label>
-          <input type="radio" name="dor" value="Dependencia" >
-          <label>Dependencia</Label>
+          <label>Qual sua principal dor ligada a relacionamentos?</label>
+          <input type="radio" name="dor" value="Dependencia">
+          <label>Dependencia</label>
 
           <input type="radio" name="dor" value="Superar relacionamentos" checked>
-          <label>Superar relacionamentos</Label>
-          
-          <input type="radio" name="dor" value="Relacionamento abusivo">
-          <label>Viver em um relacionamento abusivo</Label>
-        </div><br>
+          <label>Superar relacionamentos</label>
 
+          <input type="radio" name="dor" value="Relacionamento abusivo">
+          <label>Viver em um relacionamento abusivo</label>
+        </div><br>
+        <!-- Adicione um botão para avançar para a próxima pergunta -->
+        <button type="button" onclick="showQuestion(4)">Próximo</button>
+      </div>
+
+      <div id="question4" style="display: none;">
+        <!-- Pergunta 4 -->
         <div>
-          <label> Você está pronto para mudar de vida?</label>
-          <input type="radio" name="mudar" value="Sim" >
-          <label>Sim</Label>
+          <label>Você está pronto para mudar de vida?</label>
+          <input type="radio" name="mudar" value="Sim">
+          <label>Sim</label>
 
           <input type="radio" name="mudar" value="Não" checked>
-          <label>Não, prefiro continuar sofrendo</Label>
+          <label>Não, prefiro continuar sofrendo</label>
         </div><br>
+        <!-- Adicione um botão para avançar para a próxima pergunta -->
+        <button type="button" onclick="showQuestion(5)">Próximo</button>
+      </div>
 
+      <div id="question5" style="display: none;">
+        <!-- Pergunta 5 -->
         <div>
-          <label> Você faz terapia atualmente?</label>
-          <input type="radio" name="terapia" value="Sim" checked >
-          <label>Sim</Label>
+          <label>Você faz terapia atualmente?</label>
+          <input type="radio" name="terapia" value="Sim">
+          <label>Sim</label>
 
-          <input type="radio" name="terapia" value="Não">
-          <label>Não</Label>
+          <input type="radio" name="terapia" value="Não" checked>
+          <label>Não</label>
         </div><br>
 
+        <!-- Botão para enviar o formulário -->
         <input type="hidden" name="timestamp" value="<?php echo date('Y-m-d H:i:s'); ?>">
-        <input type="submit" value="Create">
-        <a href="read.php">View Users</a>
-        
-        <br><br><mark>
-            <?php if (isset($_GET['ms'])){
-              echo $_GET['ms'];
-            } ?>
-        </mark>
+        <button type="submit" value="Create">Enviar</button>
       </div>
     </form>
+    </div>
   </section>
-  
+
+  <script>
+    function showQuestion(questionNumber) {
+      // Esconder todas as perguntas
+      for (let i = 1; i <= 5; i++) {
+        document.getElementById('question' + i).style.display = 'none';
+      }
+
+      // Mostrar a próxima pergunta
+      if (questionNumber <= 5) {
+        document.getElementById('question' + questionNumber).style.display = 'block';
+      }
+    } 
+
+  </script>
 </body>
 </html>
